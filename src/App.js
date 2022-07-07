a03e6acd3a42bea63fe6bad3c129ccf93b99fdec
Samiya <skmcintosh20@gmail.com>
2022-07-07 16:23:46 -0700

    Complete hero w/ play functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
 import Netflix from "./pages/Netflix";
+import Player from "./pages/Player";
 import Signup from "./pages/Signup";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route exact path="/login" element={<Login />} />
 				<Route exact path="/signup" element={<Signup />} />
+				<Route exact path="player" element={<Player />} />
 				<Route exact path="/" element={<Netflix />} />
 			</Routes>
 		</BrowserRouter>
