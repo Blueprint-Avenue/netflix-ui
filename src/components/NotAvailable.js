2b0c505568d254e479e47c673f972aba93343e5b
Samiya <skmcintosh20@gmail.com>
2022-07-08 14:01:00 -0700

    added movies page w genre component

---
+++ src/components/NotAvailable.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotAvailable() {
+	return (
+		<div className="not-available">
+			<h1>No Movies Available For Selected genre</h1>
+		</div>
+	);
+}
+
+export default NotAvailable;
