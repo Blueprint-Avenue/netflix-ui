700c265de88c630cd0fabe6f25027c69abd6b1b2
Samiya <skmcintosh20@gmail.com>
2022-07-08 11:51:25 -0700

    fetched/created movie genres{store}

--- src/pages/Netflix.js
+++ src/pages/Netflix.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Navbar from "../components/Navbar";
 import backgroundImage from "../assets/home.jpg";
 import movieLogo from "../assets/homeTitle.webp";
@@ -6,15 +6,33 @@ import {FaPlay} from "react-icons/fa";
 import {AiOutlineInfoCircle} from "react-icons/ai";
 import styled from "styled-components";
 import {useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchMovies, getGenres} from "../store";
 
 function Netflix() {
 	const [isScrolled, setIsScrolled] = useState(false);
+
+	const genres = useSelector((state) => state.netflix.genres);
+	const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+	const movies = useSelector((state) => state.netflix.movies);
+
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
 	window.onscroll = () => {
 		setIsScrolled(window.pageYOffset === 0 ? false : true);
 		return () => (window.onscroll = null);
 	};
+
+	useEffect(() => {
+		dispatch(getGenres());
+	}, []);
+
+	useEffect(() => {
+		if (genresLoaded) {
+			dispatch(fetchMovies({genres, type: "all"}));
+		}
+	}, [genresLoaded]);
 
 	return (
 		<Container>
